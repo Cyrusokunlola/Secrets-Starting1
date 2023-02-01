b70b18436984503888b340bfc73f94613aa78b70
Babatunde Okunlola Cyrus <babs5000b2@yahoo.com>
2023-02-01 15:12:46 +0100

    Add Hashing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 require('dotenv').config()
+const SHA256 = require("crypto-js/sha256");
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require('ejs')
@@ -21,11 +22,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",  { useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
+   
     password: String
+
 })
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] })
+
 
 const User = new mongoose.model("User", userSchema)
 
@@ -47,7 +50,7 @@ app.post("/register", function(req, res){
    
    const newUser = new User({
     email: req.body.username,
-    password:  req.body.password
+    password:  SHA256(req.body.password).toString()
    })
    
    newUser.save(function(err){
@@ -62,7 +65,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username
-    const password = req.body.password
+    const password = SHA256(req.body.password).toString()
 
     User.findOne({email: username},function(err, foundUser){
         if (err){
